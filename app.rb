2b3924a82b068e64b6ba058ec7f75973bd3b69a7
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-29 12:40:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Phong"
+  end
+
+  get '/hometown' do
+    "My hometown is Prague"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Layla"
+  end
+
 end
